105ca605f375a044c616766d15333106c975e1b0
David Waring <dev@davidwaring.net>
2021-09-17 09:00:19 -0400

    Sites: update status labels

--- src/components/sites.vue
+++ src/components/sites.vue
@@ -1,12 +1,17 @@
 <template>
     <v-card class="site-card" v-for='site in sites' v-bind:key='site.site-id'>
+        <div v-if="build && lastActivityId && siteUpdateAvailable(site)" class="site-card-update site-card-update-available">
+            <p><v-icon name="update"></v-icon>&nbsp;&nbsp;Updates Available</p>
+        </div>
+        <div v-if="build && lastActivityId && !siteUpdateAvailable(site)" class="site-card-update site-card-update-none">
+            <p><v-icon name="check_circle"></v-icon>&nbsp;&nbsp;Site Updated</p>
+        </div>
         <v-card-title>{{ site[config.keys.name] }}</v-card-title>
         <v-card-subtitle>{{ site[config.keys.url] }}</v-card-subtitle>
         <v-card-text>
             <p v-if="settings"><strong>Path:</strong> <code>{{ site[config.keys.path] }}</code></p>
             <p v-if="settings"><strong>Build Command:</strong> <code>{{ site[config.keys.command] }}</code></p>
             <p><strong>Last Updated:</strong> {{ site[config.keys.timestamp] ? site[config.keys.timestamp] : 'Unknown' }}</p>
-            <p><strong>Update?</strong> {{ siteUpdateAvailable(site) }}</p>
         </v-card-text>
         <v-card-actions>
             <v-button v-bind:href="site[config.keys.url]"><v-icon name="launch"></v-icon>&nbsp;View</v-button>
@@ -65,23 +70,33 @@
 <style scoped>
     .site-card {
         margin: 25px auto;
-        box-shadow: 0 2px 4px 0 rgba(0,0,0,0.2);
+        box-shadow: 0 2px 3px 2px rgba(0,0,0,0.1);
         transition: 0.3s;
         max-width: 500px !important;
     }
     .site-card:hover {
-        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+        box-shadow: 0 3px 4px 3px rgba(0,0,0,0.1);
+    }
+    .site-card-update {
+        padding: 5px 10px;
+    }
+    .site-card-update-available {
+        background-color: var(--warning);
+        color: var(--warning-alt);
+    }
+    .site-card-update-none {
+        background-color: var(--success);
+        color: var(--success-alt);
     }
     .site-card code {
         font-family: Monaco, monospace;
         line-height: 100%;
-        background-color: #eee;
         padding: 0.2em;
     }
     .site-card .v-button.danger {
-		--v-button-color: var(--red);
-		--v-button-background-color: var(--red-50);
-		--v-button-color-hover: var(--white);
-		--v-button-background-color-hover: var(--red);
+		--v-button-color: var(--danger-alt);
+		--v-button-background-color: var(--danger);
+		--v-button-color-hover: var(--danger-alt);
+		--v-button-background-color-hover: var(--danger-125);
 	}
 </style>
